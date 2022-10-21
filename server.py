24145bc8f52ce7d76adf28eee14ace9e3095e131
Shubhp333 <shubh.123478@gmail.com>
2022-10-21 16:42:17 -0400

    updated

--- server.py
+++ server.py
@@ -13,5 +13,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             Secret_key = conn.recv(1024)
             with open('random_key.txt', 'wb') as f:
                 f.write(Secret_key)
+
+
+                with open (random_key.txt, "rb") as thefile:
+                     contents = thefile.read()
+                contents_encrypted = Fernet(key).encrypt(contents)
+                with open(file, "wb") as thefile:
+                    thefile.write(contents_encrypted)
+
+
             break
         print('connection completed and key recived')
